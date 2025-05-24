5313878fe24a21ea9b04b13689851a4448bf228c
Rehatpreet kaur <rehatpreetkaur981@gmail.com>
2025-05-24 04:51:14 -0400

    New job for github actions

---
+++ tests/unit/app.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe("GET /some-unknoenwn-endpoint", () => {
+    test("undefined endpoint should return 404", async () => {
+        const res = await request(app).get("/some-unknoenwn-endpoint");
+        expect(res.statusCode).toBe(404);
+        expect(res.body.status).toEqual("error");
+        expect(res.body.error.code).toEqual(404);
+        expect(res.body.error.message).toEqual("not found");
+    }
+    );
+})
