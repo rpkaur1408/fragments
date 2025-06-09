7e3dc298840c7d184dc1fbca297371deb41085e3
Rehatpreet kaur <rehatpreetkaur981@gmail.com>
2025-06-09 19:12:51 -0400

    Add memory-db

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -5,9 +5,9 @@ const { createSuccessResponse} = require('../../response');
  * Get a list of fragments for the current user
  */
 module.exports = (req, res) => {
-  // TODO: this is just a placeholder. To get something working, return an empty array...
+
   res.status(200).json(createSuccessResponse({
-    // TODO: change me
+
     fragments: [],
   }));
 };
