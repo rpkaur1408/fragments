793cdc0f63cfff9643778d885140875c44af03ff
Rehatpreet kaur <rehatpreetkaur981@gmail.com>
2025-06-10 21:12:56 -0400

    removing extra code

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,33 +1,6 @@
-// // src/routes/api/get.js
-// const { Fragment } = require('../../model/fragment');
-
-// /**
-//  * Get a list of fragments for the current user
-//  */
-// // module.exports = (req, res) => {
-// //   const fragments = await Fragment.byUser(req.user);
-
-// //   res.status(200).json(createSuccessResponse({
-// //     status: 'ok',
-// //     fragments: [],
-// //   }));
-// // };
-
-// module.exports = async (req, res) => {
-//   // Get all fragments metadata for the authenticated user
-//   const fragments = await Fragment.byUser(req.user);
-
-//   res.status(200).json({
-//     status: 'ok',
-//     fragments,
-//   });
-// };
-
-
+//GET routes 
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 const logger = require('../../logger');
-
-
 
 const { Fragment } = require('../../model/fragment');
 
