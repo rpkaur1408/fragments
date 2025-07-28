f0d43537b7ce64996690c9ecc835e9e8fe5741bb
Rehatpreet kaur <rehatpreetkaur981@gmail.com>
2025-07-27 20:04:25 -0400

    Add hostname to health check response - Release v0.8.6

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 // src/routes/index.js
 const { createSuccessResponse} = require('../response');
 const express = require('express');
+const { hostname } = require('os');
 
 // version and author from package.json
 const { version, author } = require('../../package.json');
@@ -19,15 +20,17 @@ router.use(`/v1`, authenticate(), require('./api'));
  * we'll respond with a 200 OK.  If not, the server isn't healthy.
  */
 router.get('/', (req, res) => {
-  // Client's shouldn't cache this response (always request it fresh)
+ 
   res.setHeader('Cache-Control', 'no-cache');
-  // Send a 200 'OK' response
+
   res.status(200).json(createSuccessResponse({
 
-    author,
-    // Use your own GitHub URL for this!
+    author:"Rehatpreet Kaur",
+
     githubUrl: 'https://github.com/rpkaur1408/fragments',
     version,
+    // Include the hostname in the response
+    hostname: hostname(),
   }));
 });
 
